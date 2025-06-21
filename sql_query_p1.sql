1b741ebfb24e659fa9c545bbfcafeea098a4e51e
Ibrahim Ahmed <148594575+Ibrah1m18@users.noreply.github.com>
2025-06-21 22:42:53 +0300

    Update sql_query_p1.sql

--- sql_query_p1.sql
+++ sql_query_p1.sql
@@ -78,14 +78,11 @@ WHERE
     
 -- Data Exploration
 
--- How many sales we have?
+-- How many sales do we have?
 SELECT COUNT(*) as total_sale FROM retail_sales
 
--- How many uniuque customers we have ?
-
+-- How many unique customers do we have?
 SELECT COUNT(DISTINCT customer_id) as total_sale FROM retail_sales
-
-
 
 SELECT DISTINCT category FROM retail_sales
 
@@ -94,34 +91,33 @@ SELECT DISTINCT category FROM retail_sales
 
 -- My Analysis & Findings
 -- Q.1 Write a SQL query to retrieve all columns for sales made on '2022-11-05
--- Q.2 Write a SQL query to retrieve all transactions where the category is 'Clothing' and the quantity sold is more than 10 in the month of Nov-2022
+-- Q.2 Write a SQL query to retrieve all transactions where the category is 'Clothing' and the quantity sold is more than 10 in November 2022
 -- Q.3 Write a SQL query to calculate the total sales (total_sale) for each category.
 -- Q.4 Write a SQL query to find the average age of customers who purchased items from the 'Beauty' category.
--- Q.5 Write a SQL query to find all transactions where the total_sale is greater than 1000.
+-- Q.5 Write a SQL query to find all transactions where the total sale is greater than 1000.
 -- Q.6 Write a SQL query to find the total number of transactions (transaction_id) made by each gender in each category.
--- Q.7 Write a SQL query to calculate the average sale for each month. Find out best selling month in each year
+-- Q.7 Write a SQL query to calculate the average sale for each month. Find out best best-selling month in each year
 -- Q.8 Write a SQL query to find the top 5 customers based on the highest total sales 
 -- Q.9 Write a SQL query to find the number of unique customers who purchased items from each category.
--- Q.10 Write a SQL query to create each shift and number of orders (Example Morning <=12, Afternoon Between 12 & 17, Evening >17)
+-- Q.10 Write a SQL query to create each shift and number of orders (Example: Morning <=12, Afternoon Between 12 & 17, Evening >17)
 
 
 
- -- Q.1 Write a SQL query to retrieve all columns for sales made on '2022-11-05
+ --  Write a SQL query to retrieve all columns for sales made on '2022-11-05
 
 SELECT *
 FROM retail_sales
 WHERE sale_date = '2022-11-05';
 
 
--- Q.2 Write a SQL query to retrieve all transactions where the category is 'Clothing' and the quantity sold is more than 4 in the month of Nov-2022
+--  Write a SQL query to retrieve all transactions where the category is 'Clothing' and the quantity sold is equal to or more than 4 in November 2022
 
-SELECT 
-  *
+SELECT *
 FROM retail_sales
 WHERE 
     category = 'Clothing'
     AND 
-    TO_CHAR(sale_date, 'YYYY-MM') = '2022-11'
+    sale_date like '2022-11%'
     AND
     quantity >= 4
 
@@ -143,7 +139,7 @@ FROM retail_sales
 WHERE category = 'Beauty'
 
 
--- Q.5 Write a SQL query to find all transactions where the total_sale is greater than 1000.
+-- Q.5 Write a SQL query to find all transactions where the total sale is greater than 1000.
 
 SELECT * FROM retail_sales
 WHERE total_sale > 1000
@@ -163,7 +159,7 @@ GROUP
 ORDER BY 1
 
 
--- Q.7 Write a SQL query to calculate the average sale for each month. Find out best selling month in each year
+-- Q.7 Write a SQL query to calculate the average sale for each month. Find out best best-selling month in each year
 
 SELECT 
        year,
@@ -180,8 +176,7 @@ FROM retail_sales
 GROUP BY 1, 2
 ) as t1
 WHERE rank = 1
-    
--- ORDER BY 1, 3 DESC
+ORDER BY 1, 3 DESC
 
 -- Q.8 Write a SQL query to find the top 5 customers based on the highest total sales 
 
